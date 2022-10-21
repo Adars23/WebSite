1996d61d8bf8a2b6ab3c85e8353a06f7cd724d3f
Aadarsha Shakya <aadarshakya23@gmail.com>
2022-10-21 16:57:14 +0545

    first commit

---
+++ Development/java/js.java
@@ -0,0 +1,80 @@
+
+function img_changer1()
+{
+	var a = document.getElementById('novo');
+	var b = document.getElementById('foot');
+	if (a.style.display == "none") 
+		{
+			a.style.display = "block";	
+			b.style.display = "none";
+		}
+	else
+		{
+			b.style.display = "block";
+			a.style.display = "none";
+		}
+
+}
+function img_changer2()
+{
+	var a = document.getElementById('vim');
+	var b = document.getElementById('home');
+	if (a.style.display == "none") 
+		{
+			a.style.display = "block";	
+			b.style.display = "none";
+		}
+	else
+		{
+			b.style.display = "block";
+			a.style.display = "none";
+		}
+}
+function img_changer3()
+{
+	var a = document.getElementById('moz');
+	var b = document.getElementById('nav');
+	if (a.style.display == "none") 
+	{
+		a.style.display = "block";	
+		b.style.display = "none";
+	}
+	else
+	{
+		b.style.display = "block";
+		a.style.display = "none";
+	}
+
+}
+function img_changer4()
+{
+	var a = document.getElementById('sb');
+	var b = document.getElementById('blog');
+	if (a.style.display == "none") 
+		{
+				a.style.display = "block";	
+				b.style.display = "none";
+		}
+	else
+		{
+			b.style.display = "block";
+			a.style.display = "none";
+		}
+}	
+function img_changer5()
+{
+	var a = document.getElementById('google');
+	var b = document.getElementById('cu');
+	if (a.style.display == "none") 
+		{
+			a.style.display = "block";	
+			b.style.display = "none";
+		}
+	else
+		{
+			b.style.display = "block";
+			a.style.display = "none";
+		}
+
+}
+			
